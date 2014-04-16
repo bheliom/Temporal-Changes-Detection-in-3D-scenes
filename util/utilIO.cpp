8b8326ecebabc9001882d4d06a64b1bb847cc167
bheliom <waldemar.franczak@gmail.com>
2014-04-16 16:24:11 +0900

    mask projection bug fixing

--- util/utilIO.cpp
+++ util/utilIO.cpp
@@ -51,11 +51,14 @@ void ImgIO::projChngMaskTo3D(cv::Mat chngMask, vcg::Shot<float> cam1, vcg::Shot<
     cam2_Rt.at<int>(i,3) = cam2_tra[i];
   }
 
+  cam1_intr.at<int>(0,0) = 5;
+
+  std::cout<<"czy to tu?"<<std::endl;
 
   cam1_intr.at<int>(0,0) = static_cast<float>(cam1.Intrinsics.FocalMm);
   cam1_intr.at<int>(1,1) = static_cast<float>(cam1.Intrinsics.FocalMm);
 
-  std::cout<<"czy to tu?"<<std::endl;
+
 
   cam1_intr.at<int>(0,2) = cam1.Intrinsics.CenterPx[0];
   cam1_intr.at<int>(1,2) = cam1.Intrinsics.CenterPx[1];
