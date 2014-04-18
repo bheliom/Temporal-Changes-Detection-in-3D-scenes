49d94ffe470f8bc5b6da7e84039b21475369a0e0
bheliom <waldemar.franczak@gmail.com>
2014-04-18 12:52:29 +0900

    fixing mask projection

--- util/utilIO.cpp
+++ util/utilIO.cpp
@@ -100,7 +100,7 @@ cv::Mat ImgIO::projChngMaskTo3D(const cv::Mat &chngMask, const vcg::Shot<float> 
 
   cam1_fmat = cam1_intr*cam1_Rt;
   cam2_fmat = cam2_intr*cam2_Rt;
-  std::cout<<"P matrix 1:"<<cam1_fmat<<" P matrix 2:"<<cam2_mat<<std::endl;
+  std::cout<<"P matrix 1:"<<cam1_fmat<<" P matrix 2:"<<cam2_fmat<<std::endl;
 
   cv::perspectiveTransform(cam1_points, cam2_points, H);
   
