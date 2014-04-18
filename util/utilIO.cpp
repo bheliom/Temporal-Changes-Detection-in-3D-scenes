3d08bfe5a5455ad64dc1cac26ab774b02b3fcd3e
bheliom <waldemar.franczak@gmail.com>
2014-04-18 15:27:29 +0900

    fixing mask projection

--- util/utilIO.cpp
+++ util/utilIO.cpp
@@ -7,6 +7,7 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <ctime>
 
+
 void MeshIO::saveChngMask3d(const std::vector<cv::Mat> &pts_3d, const std::string &name){
 
   MyMesh m;
@@ -24,7 +25,8 @@ void MeshIO::saveChngMask3d(const std::vector<cv::Mat> &pts_3d, const std::strin
       tmpMat  = pts_3d[i].col(c);
       w = tmpMat.at<float>(3,0);
       
-      vcg::tri::Allocator<MyMesh>::AddVertex(m, MyMesh::CoordType(tmpMat.at<float>(0,0)/w,tmpMat.at<float>(1,0)/w,tmpMat.at<float>(2,0)/w));
+      vcg::tri::Allocator<MyMesh>::AddVertex(m, MyMesh::CoordType(tmpMat.at<float>(0,0),tmpMat.at<float>(1,0),tmpMat.at<float>(2,0)));
+
       m.vert[c].SetS();
     }
 
