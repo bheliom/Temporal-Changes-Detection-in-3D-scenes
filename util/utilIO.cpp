59bb6b821824a1e066321390159d96f646da405b
bheliom <waldemar.franczak@gmail.com>
2014-04-21 13:41:59 +0900

    fixing mask projection

--- util/utilIO.cpp
+++ util/utilIO.cpp
@@ -103,7 +103,7 @@ void ImgIO::getPtsFromMask(const cv::Mat &mask, std::vector<cv::Point2f> &pts_ve
 
   for(int r = 0; r < rows; r++){
     for(int c = 0; c < cols; c++){      
-      std::cout<< mask.at<float>(r,c) << std::endl;
+      std::cout<< mask.at<uchar>(r,c) << std::endl;
       if(mask.at<double>(r,c)>0){
 	pts_vector.push_back(cv::Point2f(r,c));
       }
