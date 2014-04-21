3f29a193d6f474231ffdc40da80a621a2acb47e1
bheliom <waldemar.franczak@gmail.com>
2014-04-21 15:15:43 +0900

    fixing mask projection

--- util/utilIO.cpp
+++ util/utilIO.cpp
@@ -117,7 +117,7 @@ cv::Mat ImgIO::getRtMatrix(const vcg::Shot<float> &shot){
   cv::Mat mat_Rt(3,4, CV_64FC1);
   mat_Rt = cv::Mat::zeros(3,4, CV_64FC1);
 
-  vcg::Matrix44f mat_rot = shot.GetWorldToExtrinsicsMatrix();
+  vcg::Matrix44f mat_rot = shot.GetExtrinsicsToWorldMatrix();
   
   for(int i = 0 ; i < 3 ; i++){
     for(int j = 0 ; j < 4 ; j++){
