76d1222553c0766620cce4829ec37408fb0d2363
bheliom <waldemar.franczak@gmail.com>
2014-04-24 17:28:34 +0900

    new projecting technique fix

--- util/utilIO.cpp
+++ util/utilIO.cpp
@@ -149,23 +149,23 @@ std::vector<vcg::Point3f> ImgIO::projChngMask(const std::string &filename, const
     DrawProgressBar(40, static_cast<double>(double(i)/double(mask_pts.size())));
 
     Eigen::Vector4f direction;
-    vcg::Point3f tmp_dir = shot.UnProject(vcg::Point2f(mask_pts[i].x, mask_pts[i].y), 1);
+    vcg::Point3f tmp_dir = shot.UnProject(vcg::Point2f(mask_pts[i].x, mask_pts[i].y), 10);
 
     tmp_dir.ToEigenVector(direction);
 
-    float tmp_mp = voxel_grid.getBoxIntersection(origin,100*direction);
+    float tmp_mp = voxel_grid.getBoxIntersection(origin, direction);
       
     if(tmp_mp==-1)
       continue;
 
-    direction = origin + tmp_mp/100*direction;
+    direction = origin + tmp_mp*direction;
 
     Eigen::Vector3i vox_coord = voxel_grid.getGridCoord(direction[0],direction[1],direction[2]);     
-    float mp_factor = 1.5;
+    float mp_factor = -1.5;
     int is_occ = 0;
     int cnt = 0;
 
-    while(is_occ==0 && cnt<100){
+    while(is_occ==0 && cnt<10){
 
       direction = origin + mp_factor*direction;
   
